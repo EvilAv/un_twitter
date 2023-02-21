99eb9fc019c5b2f332148c080b0a841bdbb7535d
EvilAv <andrey.vookolov85@gmail.com>
2023-02-21 17:48:24 +0300

    fix secret key leak

--- un_twitter/un_twitter/settings.py
+++ un_twitter/un_twitter/settings.py
@@ -1,3 +1,10 @@
+import environ
+import os
+
+env = environ.Env(
+    # DEBUG=(bool, False)
+)
+
 """
 Django settings for un_twitter project.
 
@@ -14,13 +21,13 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uo$9w02dpzswe^)pw%7&13%-q%ux2n1&esrhy2wr4^g+8lixpo'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
