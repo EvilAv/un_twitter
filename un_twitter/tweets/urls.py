8101296c9d0f4ca5d89c52a4c2230129fcd0ac46
EvilAv <andrey.vookolov85@gmail.com>
2023-04-15 01:18:12 +0300

    dynamic load test succes
    dont forget to clean allt these things

--- un_twitter/tweets/urls.py
+++ un_twitter/tweets/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_page, name='home')
+    path('', views.home_page, name='home'),
+    path('test<int:start>', views.test_view),
+    path('test-list', views.test_list_view, name='test-list'),
 ]
--- un_twitter/tweets/views.py
+++ un_twitter/tweets/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Test
 
 
 # Create your views here.
 def home_page(request):
     return render(request, 'tweets/index.html')
+
+
+def test_view(request, start):
+    if start >= len(Test.objects.all()):
+        return JsonResponse({})
+    elif start + 40 >= len(Test.objects.all()):
+        end = len(Test.objects.all()) - 1
+    else:
+        end = start + 40
+    test_list = Test.objects.all()[start:end]
+    json_list = [{'text': i.text} for i in test_list]
+    data = {
+        'name': 'aaaa',
+        'data': json_list,
+    }
+    return JsonResponse(data)
+
+
+def test_list_view(request):
+    return render(request, 'tweets/test.html')
